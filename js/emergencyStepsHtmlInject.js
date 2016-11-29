9075cd9fc7a9b8b5eed2be202cb003ee169ba9a0
Jeremy Reynolds <jeremy.reynolds@microsoft.com>
2016-11-29 14:19:44 -0700

    first draft of hosting the mapping page

---
+++ js/emergencyStepsHtmlInject.js
@@ -0,0 +1,31 @@
+﻿var processCoopDivs = function () {
+    var config = {
+        filePath1: "../info/EmergencySteps1.txt",
+        filePath2: "../info/EmergencySteps2.txt",
+        filePath3: "../info/EmergencySteps3.txt"
+    };
+
+    populateDivs = function () {
+        readTextFile(config.filePath1, '#step1')
+        readTextFile(config.filePath2, '#step2')
+        readTextFile(config.filePath3, '#step3')
+    };
+
+    readTextFile = function (filePath, divPlaceholder) {
+        $.get(filePath, function (textContents) {
+            $(divPlaceholder).html(textContents)
+        }, 'text');
+    };
+
+   return {
+       populateDivs: populateDivs
+    }
+}();
+
+$(document).ready(function () {
+    StartPageScript();
+});
+
+function StartPageScript() {
+    processCoopDivs.populateDivs();
+}
